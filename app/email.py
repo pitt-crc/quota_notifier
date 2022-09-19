214399a0422c4cd32befae65216a819748e52b0b
Daniel <djperrefort@gmail.com>
2022-09-19 18:39:20 -0400

    Fix typo in method signatures

--- app/email.py
+++ app/email.py
@@ -1,7 +1,10 @@
-"""Formattable email template for sending quota notifications to users."""
+"""Formattable email template for sending quota notifications to users.
+By default, emails are sent via the SMTP server running on localhost.
+"""
 
 from email.message import EmailMessage
 from smtplib import SMTP
+from typing import Optional
 
 
 class EmailTemplate:
@@ -16,23 +19,30 @@ class EmailTemplate:
         quota_str = '\n'.join(map(str, quotas))
         self.message = '\n\n'.join((self.header, quota_str, self.footer))
 
-    def send_to_user(self, user, from_="no-reply@crc.pitt.edu", subject="CRC Disk Usage Update"):
+    def send_to_user(
+            self,
+            user: str,
+            ffrom: str = "no-reply@crc.pitt.edu",
+            subject: str = "CRC Disk Usage Update",
+            smtp: Optional[SMTP] = None
+    ) -> EmailMessage:
         """Send the formatted email to the given username
 
         Args:
             user: Name of the user to send to
-            from_: Sender address
+            ffrom: Sender address
             subject: Email subject line
+            smtp: Optionally use a custom SMTP server
         """
 
-        self.send(to=f'{user}@pitt.edu', from_=from_, subject=subject)
+        return self.send(to=f'{user}@pitt.edu', ffrom=ffrom, subject=subject, smtp=smtp)
 
-    def send(self, to, from_, subject, smtp) -> EmailMessage:
+    def send(self, to: str, ffrom: str, subject: str, smtp: Optional[SMTP] = None) -> EmailMessage:
         """Send the formatted email to the given email address
 
         Args:
             to: Destination address
-            from_: Sender address
+            ffrom: Sender address
             subject: Email subject line
             smtp: Optionally use a custom SMTP server
         """
@@ -40,10 +50,10 @@ class EmailTemplate:
         email = EmailMessage()
         email.set_content(self.message)
         email["Subject"] = subject
-        email["From"] = from_
+        email["From"] = ffrom
         email["To"] = to
 
-        with  smtp or SMTP("localhost") as smtp_server:
+        with smtp or SMTP("localhost") as smtp_server:
             smtp_server.send_message(email)
 
         return email
