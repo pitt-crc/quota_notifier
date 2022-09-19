6cb3527d29082fdd773e8b9a3f5a535f1bd6ecef
Daniel Perrefort <djperrefort@gmail.com>
2022-09-19 18:58:33 -0400

    First pass at user notification logic (#7)
    * Add method for fetching collection of user quotas
    
    * Expand disk_utils module docs
    
    * Add basic logic for triggering email notifications
    
    * Add placeholder function for determining next threshold
    
    * Add missing import statement
    
    * Fix trailing parenthesis

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 """Commandline interface and entrypoint for the parent package"""
 
 from argparse import ArgumentParser
+from pathlib import Path
 
 from . import __version__
+from .disk_utils import AbstractQuota, BeegfsQuota, GenericQuota, IhomeQuota
+from .email import EmailTemplate
+from .shell import User
 
 
 class Parser(ArgumentParser):
@@ -26,14 +30,72 @@ class Parser(ArgumentParser):
 class Application:
     """Entry point for instantiating and executing the application from the command line"""
 
-    def send_notifications(self) -> None:
-        """Send email notifications to any users who have exceeded a notification threshold
+    @staticmethod
+    def _get_users() -> tuple[User]:
+        """Return a collection of users to check quotas for
 
-        Emails are only sent for a given user and threshold if a previous email
-        has not already been issued.
+        Returns:
+            A tuple of ``User`` objects
         """
 
         raise NotImplementedError
+
+    def _get_next_threshold(self, quota: AbstractQuota) -> int:
+        """Return the next threshold a user should be notified for
+
+        Args:
+            quota: The quota to get a threshold for
+
+        Returns:
+            A notification threshold between 0 and 100 (inclusive)
+        """
+
+        raise NotImplementedError
+
+    @staticmethod
+    def _get_user_quotas(user: User) -> tuple[AbstractQuota]:
+        """Return a tuple of quotas assigned to a given user
+
+        Args:
+            user: The user to fetch quotas for
+
+        Returns:
+            A (possibly empty) tuple of quota objects
+        """
+
+        ihome_quota = IhomeQuota.from_uid('ihome', user.uid)
+        zfs1_quota = GenericQuota.from_path('zfs1', Path(f'/zfs1/{user.group}'))
+        zfs2_quota = GenericQuota.from_path('zfs2', Path(f'/zfs2/{user.group}'))
+        bgfs_quota = BeegfsQuota.from_group('beegfs', user.group)
+        ix_quota = GenericQuota.from_path('ix', Path(f'/ix/{user.group}'))
+        ix1_quota = GenericQuota.from_path('ix1', Path(f'/ix1/{user.group}'))
+
+        # Only return quotas that exist for the given group (i.e., objects that are not None)
+        all_quotas = (ihome_quota, zfs1_quota, zfs2_quota, bgfs_quota, ix_quota, ix1_quota)
+        return tuple(filter(None, all_quotas))
+
+    def _notify_user(self, user: User) -> None:
+        """Send email notifications to a single user
+
+        Args:
+            user: The user to send a notification for
+        """
+
+        pending_notifications = []
+        for quota in self._get_user_quotas(user):
+            next_threshold = self._get_next_threshold(quota)
+            usage = (quota.size_used * 100) // quota.size_limit
+            if usage >= next_threshold:
+                pending_notifications.append(quota)
+
+        if pending_notifications:
+            EmailTemplate(pending_notifications).send_to_user(user)
+
+    def send_notifications(self) -> None:
+        """Send email notifications to any users who have exceeded a notification threshold"""
+
+        for user in self._get_users():
+            self._notify_user(user)
 
     @classmethod
     def execute(cls) -> None:
