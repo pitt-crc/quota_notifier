b4cd3dbd039651ee61002a35d4df368c6caffa41
Daniel <djperrefort@gmail.com>
2022-09-19 17:15:46 -0400

    Add placeholder function

--- app/main.py
+++ app/main.py
@@ -4,6 +4,15 @@
 class Application:
     """Entry point for instantiating and executing the application from the command line"""
 
+    def process_notifications(self) -> None:
+        """Send email notifications to any users who have exceeded a notification threshold
+
+        Emails are only sent for a given user and threshold if a previous email
+        has not already been issued.
+        """
+
+        raise NotImplementedError
+
     @classmethod
     def execute(cls):
         """Parse arguments and execute the application"""
