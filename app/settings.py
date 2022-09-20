cc4517313fb4d6f4da3c346d7b134fa6ddc42f66
Daniel Perrefort <djperrefort@gmail.com>
2022-09-19 21:36:21 -0400

    Add dedicated settings module (#12)
    * Add dedicated class for loading settings
    
    * Update main module to use new settings module
    
    * Update signatures in test suite
    
    * Drop config class

---
+++ app/settings.py
@@ -0,0 +1,25 @@
+"""Load and manage settings for the parent application"""
+
+from pathlib import Path
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class FileSystem(BaseSettings):
+    """Settings for an individual file system"""
+
+    name: str
+    path: str
+    type: str
+
+
+class Settings(BaseSettings):
+    """Top level settings object for the parent application"""
+
+    ihome_quota_path: Path = Path('/ihome/crc/scripts/ihome_quota.json')
+    thresholds: tuple[int, ...] = (75, 100)
+    file_systems: Optional[tuple[FileSystem, ...]]
+
+
+app_settings = Settings()
