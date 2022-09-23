bb0f62096df1c8f6d73bc6328327eee659dad657
Daniel <djperrefort@gmail.com>
2022-09-23 09:14:05 -0400

    Update email template with reviewer comments

--- app/settings.py
+++ app/settings.py
@@ -23,14 +23,15 @@ class Settings(BaseSettings):
 
     # Email notification settings
     email_from: str = 'no-reply@crc.pitt.edu'
-    email_subject: str = "CRC Disk Usage Update"
+    email_subject: str = 'CRC Disk Usage Alert'
     email_header = (
         "This is an automated notification concerning your storage quota on H2P. "
         "One or more of your quotas have surpassed a usage threshold triggering an automated notification. "
         "Your storage usage is as follows:")
 
     email_footer = (
-        "If you need additional storage, please submit a request via the CRC ticketing system.\n\n"
+        "If you need additional storage, please submit a request via the CRC ticketing system. "
+        "Our storage policies are described in https://crc.pitt.edu/user-support/data-storage-guidelines.\n\n"
         "Sincerely,\n"
         "The CRC Quota Bot"
     )
