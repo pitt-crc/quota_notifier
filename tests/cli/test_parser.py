5f953025cfdc810592380102f822d99d9f5e45f2
Daniel <djperrefort@gmail.com>
2022-09-26 12:43:54 -0400

    Add tests for parser defaults

--- tests/cli/test_parser.py
+++ tests/cli/test_parser.py
@@ -2,7 +2,7 @@
 
 from unittest import TestCase
 
-from app.cli import Parser
+from app.cli import DEFAULT_SETTINGS_PATH, Parser
 
 
 class ParserHelpData(TestCase):
@@ -12,3 +12,26 @@ class ParserHelpData(TestCase):
         """Test the application name is configured as ``notifier``"""
 
         self.assertEqual('notifier', Parser().prog)
+
+    def test_has_description(self) -> None:
+        """Test the application description is not empty"""
+
+        self.assertTrue(Parser().description)
+
+
+class ParserDefaults(TestCase):
+    """Test the value of default arguments"""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.args = Parser().parse_args([])
+
+    def test_default_config_path(self) -> None:
+        """Test the default settings path matches globally defined values"""
+
+        self.assertEqual(DEFAULT_SETTINGS_PATH, self.args.settings)
+
+    def test_check_flag_false(self) -> None:
+        """Test the ``check`` flag defaults to ``False``"""
+
+        self.assertFalse(self.args.check)
