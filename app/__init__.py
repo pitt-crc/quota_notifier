465c71f9ff672da8b1d4472661680511ca354dc6
Daniel <djperrefort@gmail.com>
2022-09-26 11:10:41 -0400

    Add how it works section

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,18 @@ The Quota Notification Utility automatically scans attached file systems and
 issues email notifications to users exceeding a certain percentage of their
 storage quota. The tool is easily configurable and supports multiple file
 system types and various notification settings.
+
+How it Works
+------------
+
+The Quota Notification Utility automatically iterates through all users listed
+in the local SSSD and all filesystems listed in the application settings file.
+If a user has met or exceeded a configurable percentage for any of their storage
+quotas, they are issued an email detailing their current usage.
+
+Email notifications are only issued once per filesystem/threshold combination.
+However, a user will receive additional notifications if their usage drops below
+a threshold before exceeding the threshold a second time.
 """
 
 __version__ = '0.0.1'
