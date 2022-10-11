b174b0194e3903f0d44aadadc19823d3d2ebb270
Daniel <djperrefort@gmail.com>
2022-10-11 15:11:52 -0400

    Fixes out of date method name

--- tests/disk_utils/test_abstractquota.py
+++ tests/disk_utils/test_abstractquota.py
@@ -26,7 +26,7 @@ class BytesToString(TestCase):
     def test_zero_bytes(self) -> None:
         """Test the conversion of zero bytes"""
 
-        self.assertEqual('0.0 B', AbstractQuota.bytes_to_str(0))
+        self.assertEqual('0.0 B', AbstractQuota._bytes_to_str(0))
 
     def test_integer_conversion(self) -> None:
         """Test the conversion from bytes to known values that do not have trailing decimals"""
@@ -35,7 +35,7 @@ class BytesToString(TestCase):
         outputs = ('1.0 B', '1.0 KB', '1.0 MB', '1.0 GB', '1.0 TB', '1.0 PB', '1.0 EB', '1.0 ZB', '1.0 YB')
 
         for inp, oup in zip(inputs, outputs):
-            self.assertEqual(oup, AbstractQuota.bytes_to_str(inp))
+            self.assertEqual(oup, AbstractQuota._bytes_to_str(inp))
 
     def test_non_integer_values(self) -> None:
         """Test the conversion from bytes to known values with trailing decimals"""
@@ -44,7 +44,7 @@ class BytesToString(TestCase):
         outputs = ('488.28 KB', '47.68 MB', '4.66 GB', '4.55 TB')
 
         for inp, oup in zip(inputs, outputs):
-            self.assertEqual(oup, AbstractQuota.bytes_to_str(inp))
+            self.assertEqual(oup, AbstractQuota._bytes_to_str(inp))
 
 
 class StringRepresentation(TestCase):
